f2a967460b974b481820f2ec3b1db65d5b666ec0
Jared <jared97martinez@gmail.com>
2019-03-28 19:48:34 -0700

    Get income cards function
    Set up a function to retrieve the income cards for user who are logged in

---
+++ server/config-example.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Configuration file - EXAMPLE
+ */
+
+/**
+ * Database constants
+ */
+
+const SERVER = 'localhost';
+const DATABASE = '';
+const DB_USER = '';
+const DB_PASSWORD = '';
--- server/index.php
+++ server/index.php
@@ -18,9 +18,12 @@ if($_SERVER['REQUEST_METHOD'] !== 'POST' || !isset($_POST['request_type'])) {
 }
 
 class Finance {
+
     public static function instance()
     {
         session_start(); // Getting session information
+
+        require_once('config.php'); // Requiring database configuration file
 
         switch($_POST['request_type']){
             case 'user_logged_in':
@@ -45,6 +48,38 @@ class Finance {
         return isset($_SESSION['user_id']);
     }
 
+    /**
+     * Function to get the income cards of a user
+     * 
+     * @return array
+     */
+    private static function get_income_cards()
+    {
+        // Making sure the user is logged in
+        if(!self::is_user_logged_in) self::output(500, 'message', 'User not logged in.');
+
+        $user_id = $_SESSION['user_id']; // user id
+
+        // Database connections
+        $db = new mysqli(
+            SERVER,
+            DB_USER,
+            DB_PASSWORD,
+            DATABASE
+        );
+
+        if ($db->connect_errno) self::output(500, 'message', 'Can\'t connect to the database'); // Database error
+
+        // Query
+        $query = "SELECT    income_cards 
+                  FROM      users 
+                  WHERE     user_id = $user_id
+        ";
+
+        if(!$cards = $db->query($query)) self::output(500, 'message', 'Can\'t retrieve cards from the database'); // Error retrieving cards
+
+        self::output(200, 'income_cards', $cards); // Outputting cards
+    }
     /**
      * Function to output information the the requester
      */
